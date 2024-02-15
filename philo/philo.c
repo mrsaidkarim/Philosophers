92ccc2a449cf6b73de30d23b4e8f327686a73ee9
SaidKarim-web <sirsaidkarim@gmail.com>
2024-02-15 21:43:34 +0100

    Modify the main file

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 11:31:31 by skarim            #+#    #+#             */
-/*   Updated: 2024/02/09 16:04:28 by skarim           ###   ########.fr       */
+/*   Updated: 2024/02/15 21:43:03 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,15 +41,15 @@ int	ft_valid_input(int ac, char **av, t_data *data)
 int	ft_mutex_handler(pthread_mutex_t *mutex, t_mutex_opr mutex_opr)
 {
 	if (mutex_opr == INIT)
-		pthread_mutex_init(mutex, NULL);
+		return (pthread_mutex_init(mutex, NULL));
 	else if (mutex_opr == LOCK)
-		pthread_mutex_lock(mutex);
+		return (pthread_mutex_lock(mutex));
 	else if (mutex_opr == UNLOCK)
-		pthread_mutex_unlock(mutex);
+		return (pthread_mutex_unlock(mutex));
 	else if (mutex_opr == DESTROY)
-		pthread_mutex_destroy(mutex);
-	else 
-		return (1);
+		return (pthread_mutex_destroy(mutex));
+	else
+		return (-1);
 }
 
 int	main(int argc, char **argv)
@@ -58,14 +58,9 @@ int	main(int argc, char **argv)
 
 	if (ft_valid_input(argc, argv, &data) == -1)
 		return (ft_error_message("wrong arguments!"));
-	// printf(" number_of_philosophers: %d\n", data.nbr_philos);
-	// printf("time_to_die: %d\n", data.time_die);
-	// printf("time_to_eat: %d\n", data.time_eat);
-	// printf("time_to_sleep: %d\n", data.time_sleep);
-	// printf("number_of_times_each_philosopher_must_eat: %d\n", data.nbr_times_eat);
 	if (ft_init(&data) == -1)
-		ft_error_message("Data initialization fails");
+		return (ft_error_message("Data initialization fails"));
 	if (ft_start(&data) == -1)
-		ft_error_message("threads error");
+		return (ft_error_message("threads error"));
 	return (0);
 }
