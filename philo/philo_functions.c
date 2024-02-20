c9c576d3974168961286e353913c9c43d4e2b396
SaidKarim-web <sirsaidkarim@gmail.com>
2024-02-20 11:35:39 +0100

    handle data race

--- philo/philo_functions.c
+++ philo/philo_functions.c
@@ -6,7 +6,7 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 15:49:05 by skarim            #+#    #+#             */
-/*   Updated: 2024/02/18 16:00:06 by skarim           ###   ########.fr       */
+/*   Updated: 2024/02/19 11:37:48 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ void	ft_print(t_philo *philo, char *msg, int unlock)
 
 	time = get_time() - philo->data->start_time;
 	ft_mutex_handler(&(philo->data->writing), LOCK);
-	if (!philo->data->end && !philo->data->dead_flag)
+	if (!get_with_mutex(&philo->data->data_mtx, &philo->data->end)
+		&& !get_with_mutex(&philo->data->data_mtx, &philo->data->dead_flag))
 		printf("%ld %d %s\n", time, philo->id, msg);
 	if (unlock == 0)
 		ft_mutex_handler(&(philo->data->writing), UNLOCK);
@@ -42,9 +43,12 @@ void	ft_eat(t_philo *philo)
 	ft_mutex_handler(&philo->data->forks[philo->sfork_index].fork_mutex, LOCK);
 	ft_print(philo, "has taken a fork", 0);
 	ft_print(philo, "is eating", 0);
+	ft_mutex_handler(&philo->philo_mtx, LOCK);
 	philo->last_eating = get_time();
-	philo->is_eating = 1;
-	philo->nbr_meals++;
+	ft_mutex_handler(&philo->philo_mtx, UNLOCK);
+	set_with_mutex(&philo->philo_mtx, &philo->is_eating, 1);
+	set_with_mutex(&(philo->philo_mtx), &philo->nbr_meals,
+		philo->nbr_meals + 1);
 	ft_usleep(philo->data->time_eat);
 	ft_mutex_handler(&philo->data->forks[philo->ffork_index].fork_mutex,
 		UNLOCK);
@@ -57,10 +61,11 @@ void	*routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *) arg;
-	while (!philo->data->end && !philo->data->dead_flag)
+	while (!get_with_mutex(&philo->data->data_mtx, &philo->data->end)
+		&& !get_with_mutex(&philo->data->data_mtx, &philo->data->dead_flag))
 	{
 		ft_eat(philo);
-		philo->is_eating = 0;
+		set_with_mutex(&philo->philo_mtx, &philo->is_eating, 0);
 		ft_print(philo, "is sleeping", 0);
 		ft_usleep(philo->data->time_sleep);
 		ft_print(philo, "is thinking", 0);
