8bff3c3d553b1422216dee2e73f63cd097c8d03a
SaidKarim-web <sirsaidkarim@gmail.com>
2024-02-27 15:24:00 +0100

    Modify the header file

--- philo_bonus/philo_bonus.h
+++ philo_bonus/philo_bonus.h
@@ -6,21 +6,21 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 10:46:16 by skarim            #+#    #+#             */
-/*   Updated: 2024/02/24 16:25:23 by skarim           ###   ########.fr       */
+/*   Updated: 2024/02/25 13:23:10 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_BONUS_H
 # define PHILO_BONUS_H
 
-#include <semaphore.h>
-#include <unistd.h>
-#include <limits.h>
-#include <sys/time.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <pthread.h>
-#include <signal.h>
+# include <semaphore.h>
+# include <unistd.h>
+# include <limits.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <pthread.h>
+# include <signal.h>
 
 typedef struct s_data	t_data;
 
@@ -31,7 +31,6 @@ typedef struct s_philo
 	int			nbr_meals;
 	long		last_eating;
 	int			is_eating;
-	int			done;
 	t_data		*data;
 }	t_philo;
 
@@ -52,5 +51,10 @@ long int	ft_atoi(char *str);
 int			ft_error_message(char *str);
 long		get_time(void);
 int			ft_init(t_data *data);
+void		ft_routine(t_philo *philo);
+void		*ft_monitor(void *arg);
+void		ft_eat(t_philo *philo);
+void		ft_print(t_philo *philo, char *msg);
+void		ft_usleep(int duration);
 
 #endif
